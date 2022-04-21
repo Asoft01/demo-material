f584ff4810ff6d048633f5a2524f48b1677cd703
Adeleke Hammed Lekan <lekhad19@gmail.com>
2022-04-21 17:36:21 -0400

    Creating a reusable module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,26 @@
+import { MatComponentsModule } from './mat-components.module';
 import { CourseService } from './course.service';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
-import { MatCheckboxModule } from '@angular/material/checkbox';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
-import { MatInputModule } from '@angular/material/input';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatIconModule } from '@angular/material/icon';
-import {MatNativeDateModule} from '@angular/material/core';
-import {MatTooltipModule} from '@angular/material/tooltip';
-import {MatDialogModule} from '@angular/material/dialog';
+// import { MatCheckboxModule } from '@angular/material/checkbox';
+// import { MatRadioModule } from '@angular/material/radio';
+// import { MatSelectModule } from '@angular/material/select';
+// import { MatInputModule } from '@angular/material/input';
+// import { MatDatepickerModule } from '@angular/material/datepicker';
+// import { MatIconModule } from '@angular/material/icon';
+// import {MatNativeDateModule} from '@angular/material/core';
+// import {MatTooltipModule} from '@angular/material/tooltip';
+// import {MatDialogModule} from '@angular/material/dialog';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import {MatTabsModule} from '@angular/material/tabs';
+// import { MatButtonModule } from '@angular/material/button';
+// import { MatChipsModule } from '@angular/material/chips';
+// import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
+// import {MatTabsModule} from '@angular/material/tabs';
 import { EditCourseComponent } from './edit-course/edit-course.component';
 
 @NgModule({
@@ -35,20 +36,21 @@ import { EditCourseComponent } from './edit-course/edit-course.component';
     AppRoutingModule, 
     BrowserAnimationsModule,
     FormsModule,
+    MatComponentsModule,  
     NoopAnimationsModule,
-    MatCheckboxModule,
-    MatRadioModule, 
-    MatSelectModule,
-    MatInputModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-    MatIconModule, 
-    MatButtonModule,
-    MatChipsModule,
-    MatProgressSpinnerModule,
-    MatTooltipModule,
-    MatTabsModule,
-    MatDialogModule
+    // MatCheckboxModule,
+    // MatRadioModule, 
+    // MatSelectModule,
+    // MatInputModule,
+    // MatDatepickerModule,
+    // MatNativeDateModule,
+    // MatIconModule, 
+    // MatButtonModule,
+    // MatChipsModule,
+    // MatProgressSpinnerModule,
+    // MatTooltipModule,
+    // MatTabsModule,
+    // MatDialogModule
   ],
   providers: [
     CourseService,
